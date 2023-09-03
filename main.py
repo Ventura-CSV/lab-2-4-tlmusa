9db06fda063a3e245cbd4967266f1a8404c9e896
tlmusa <thalia_murguiasanto1@my.vcccd.edu>
2023-09-02 22:19:53 -0700

    done?

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def main():
+
+    original_str = "Python Programming"
+    sub1 = original_str[:6]      # extract the first substring 'Python'
+    sub2 = original_str[6:]      # extract the second substring 'Programming'
+    merged_str = sub2 + sub1
+
+    print(sub2)
+    print (sub1)
+    print(merged_str)
+
+
+
     ##################################################
-    # Comlete your code here
+    # Complete your code here
     ##################################################
     pass
 
